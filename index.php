d99a491edcd3cc9a17e388cbc58bd26276ec9eb5
abuAlEzzAlMushkali <thankyouplatformteam@gmail.com>
2022-09-08 23:25:37 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+//Print
+    echo 'Hello World'.'<br>';
+//Data Types
+    // String
+    $name = 'Hsoub'.'<br>';
+    // Number
+    $age = 27;
+    $pi = 3.14;
+    //Blooleans (true false)
+    $rainy = true; //Or false
+    //Null
+    $empty = null;
+    $name = null;
+
+    echo "Hello,  $name ".'<br>';
+//Convert between genres
+    echo gettype(123).'<br>';
+    //true and false
+    echo true.'<br>';
+    //Casting
+    $var = 120 .'<br>';
+    echo gettype( (string) $var ).'<br>';
+    echo (int) false.'<br>';
+    echo (bool) 500 .'<br>';
+//Constants
+    
