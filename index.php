4c188df385a624c8734be5483c0ac9ac45bbed6d
abuAlEzzAlMushkali <thankyouplatformteam@gmail.com>
2022-09-09 00:02:03 +0300

    Operators

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 //Print
-    echo 'Hello World'.'<br>';
+    echo 'Hello World' .'<br>';
 //Data Types
     // String
-    $name = 'Hsoub'.'<br>';
+    $name = 'Hsoub' .'<br>';
     // Number
     $age = 27;
     $pi = 3.14;
@@ -13,17 +13,38 @@
     $empty = null;
     $name = null;
 
-    echo "Hello,  $name ".'<br>';
+    echo "Hello,  $name " .'<br>';
 //Convert between genres
-    echo gettype(123).'<br>';
+    echo gettype(123) .'<br>';
     //true and false
-    echo true.'<br>';
+    echo true .'<br>';
     //Casting
-    $var = 120 .'<br>';
-    echo gettype( (string) $var ).'<br>';
-    echo (int) false.'<br>';
-    echo (bool) 500 .'<br>';
+    $var = 120  .'<br>';
+    echo gettype( (string) $var ) .'<br>';
+    echo (int) false .'<br>';
+    echo (bool) 500  .'<br>';
 //Constants
     const MAX_VALUE = 100;
-    echo MAX_VALUE.'<br>';
+    echo MAX_VALUE .'<br>';
 //Operators
+    $a = 4;
+    $b = 2;
+    echo $a + $b .'<br>';
+    echo $a - $b .'<br>';
+    echo $a * $b .'<br>';
+    echo $a / $b .'<br>';
+    echo $a ** $b .'<br>';
+    echo $a % $b .'<br>';
+    echo ++$a .'<br>';
+//Comparison
+    $a = 1;
+    $b = 0;
+    $c = '1';
+    $d = 0;
+    echo $a == $b .'<br>';
+    echo $c === $d .'<br>';
+    echo $a != $b .'<br>';
+    echo $a !== $b .'<br>';
+    echo $a > $b .'<br>';
+    echo $a <=> $b .'<br>';
+    
