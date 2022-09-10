a1c38f93eb46fb01e2eba0ad876594fc50356787
abuAlEzzAlMushkali <thankyouplatformteam@gmail.com>
2022-09-11 01:22:14 +0300

    conditional sentences

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
+const BR = '<br>';
 //Print
-    echo 'Hello World' .'<br>';
+    echo 'Hello World' .BR;
 //Data Types
     // String
-    $name = 'Hsoub' .'<br>';
+    $name = 'Hsoub' .BR;
     // Number
     $age = 27;
     $pi = 3.14;
@@ -13,38 +14,101 @@
     $empty = null;
     $name = null;
 
-    echo "Hello,  $name " .'<br>';
+    echo "Hello,  $name " .BR;
 //Convert between genres
-    echo gettype(123) .'<br>';
+    echo gettype(123) .BR;
     //true and false
-    echo true .'<br>';
+    echo true .BR;
     //Casting
-    $var = 120  .'<br>';
-    echo gettype( (string) $var ) .'<br>';
-    echo (int) false .'<br>';
-    echo (bool) 500  .'<br>';
+    $var = 120  .BR;
+    echo gettype( (string) $var ) .BR;
+    echo (int) false .BR;
+    echo (bool) 500  .BR;
 //Constants
     const MAX_VALUE = 100;
-    echo MAX_VALUE .'<br>';
+    
+    echo MAX_VALUE .BR;
 //Operators
     $a = 4;
     $b = 2;
-    echo $a + $b .'<br>';
-    echo $a - $b .'<br>';
-    echo $a * $b .'<br>';
-    echo $a / $b .'<br>';
-    echo $a ** $b .'<br>';
-    echo $a % $b .'<br>';
-    echo ++$a .'<br>';
+    echo $a + $b .BR;
+    echo $a - $b .BR;
+    echo $a * $b .BR;
+    echo $a / $b .BR;
+    echo $a ** $b .BR;
+    echo $a % $b .BR;
+    echo ++$a .BR;
 //Comparison
     $a = 1;
     $b = 0;
     $c = '1';
     $d = 0;
-    echo $a == $b .'<br>';
-    echo $c === $d .'<br>';
-    echo $a != $b .'<br>';
-    echo $a !== $b .'<br>';
-    echo $a > $b .'<br>';
-    echo $a <=> $b .'<br>';
-    
+    echo $a == $b .BR;
+    echo $c === $d.BR;
+    echo $a != $b .BR;
+    echo $a !== $b .BR;
+    echo $a > $b .BR;
+    echo $a <=> $b .BR;
+//conditional sentences
+    $a = 4;
+    $b = 2;
+    if ($a > $b) {
+        # code...
+        echo "<br>"."$a bigger theme $b"."<br>";
+    }
+
+    if ($a < $b) {
+        # code...
+        echo "<br>"."$a < $b"."<br>"; 
+    } else {
+        # code...
+        echo "<br>"."$a > $b"."<br>";
+    }
+    //ifelse
+    if ($a < $b) {
+        # code...
+        echo "<br>"."$a < $b"."<br>"; 
+    } else if($a > $b) {
+        # code...
+        echo "<br>"."$a > $b"."<br>";
+    }
+    //switch
+    $i = 5;
+
+    switch ($i) {
+        case 1:
+            # code...
+            echo 1;
+            break;
+        case 2:
+            # code...
+            echo 2;
+            break;
+        case 3:
+            # code...
+            echo 3;
+            break;
+        case 4:
+            # code...
+            echo 4;
+            break;
+        case 5:
+            # code...
+            echo "<br>" . 5 . "<br>";
+            break;
+        
+        default:
+            # code...
+            echo 'None of the above';
+            break;
+    }
+    // Ternary Operator
+    echo $a > $b ? "<br>"."OK"."<br>" : "<br>"."KO"."<br>";
+    //isset
+    echo "<br>".isset($a)."<br>";
+        //define
+        echo defined("MAX_VALUE") ? 'OK' : 'KO';
+    //Null Coalescing Operator
+    //$username = 'Moataz';
+    $age = 27;
+    echo  $username ??  "<br>" . $age . "<br>" ?? "<br>" . 'nobody';
